e0ec2d347458dc596a196ef01aa3689fb6fb7c54
WafflzPlz <michael.kogler1@gmx.at>
2025-02-08 23:07:44 +0100

    add visualization of grid via pygame, add pygame to requirements

--- src/__main__.py
+++ src/__main__.py
@@ -1,11 +1,70 @@
-from .generator.maze_generator import MazeGenerator
 from .maze.grid import Grid
+import pygame
 
 
 def main():
     grid = Grid(20, 20)
+    cells = grid.cells
 
-    grid.print_cells()
+    screen = pygame.display.set_mode((500, 500))
+    CELL_SIZE = 25
+
+    pygame.display.flip()
+
+    running = True
+
+    while running:
+        screen.fill((255, 255, 255))
+
+        for y in range(0, len(cells)):
+            for x in range(0, len(cells[y])):
+                walls = cells[y][x].walls
+                for wall in walls:
+                    match wall:
+                        case "n":
+                            pygame.draw.line(
+                                screen,
+                                (0, 0, 0),
+                                (CELL_SIZE * x, CELL_SIZE * y),
+                                (CELL_SIZE * x + CELL_SIZE, CELL_SIZE * y),
+                                1,
+                            )
+
+                        case "e":
+                            pygame.draw.line(
+                                screen,
+                                (0, 0, 0),
+                                (CELL_SIZE * x + CELL_SIZE, CELL_SIZE * y),
+                                (CELL_SIZE * x + CELL_SIZE,
+                                 CELL_SIZE * y + CELL_SIZE),
+                                1,
+                            )
+
+                        case "s":
+                            pygame.draw.line(
+                                screen,
+                                (0, 0, 0),
+                                (CELL_SIZE * x, CELL_SIZE * y + CELL_SIZE),
+                                (CELL_SIZE * x + CELL_SIZE,
+                                 CELL_SIZE * y + CELL_SIZE),
+                                1,
+                            )
+
+                        case "w":
+                            pygame.draw.line(
+                                screen,
+                                (0, 0, 0),
+                                (CELL_SIZE * x, CELL_SIZE * y),
+                                (CELL_SIZE * x, CELL_SIZE * y + CELL_SIZE),
+                                1,
+                            )
+
+        pygame.display.flip()
+
+        for event in pygame.event.get():
+
+            if event.type == pygame.QUIT:
+                running = False
 
 
 if __name__ == "__main__":
